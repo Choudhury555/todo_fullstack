5aebb2dde5686bd2cb34a85bced4eff029cf61dd
akashgiri2022 <akashgiri2022@gmail.com>
2024-08-24 18:57:12 +0530

    Forgot/Change password API checkin

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getMyProfile, login, logout, register } from "../controllers/user.js";
+import { forgotPassword, getMyProfile, login, logout, register } from "../controllers/user.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -11,6 +11,8 @@ router.post("/login", login);
 router.get("/logout", logout);
 
 router.get("/me", isAuthenticated, getMyProfile);//"isAuthenticated" is to check if user is logged in or not
+
+router.post("/forgotpassword",forgotPassword);
 
 
 export default router;
